eae559a5b8b6117e4dae132431a5e617fd67704d
Jonathan Cunningham <jonathan.cunning@ufl.edu>
2023-02-23 19:17:25 -0500

    Swap to SQLite3
    Removed Post method, must re-add

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,111 +1,98 @@
 package main
 
 import (
-	"crypto/sha256"											//Used for encrypting
-	"encoding/hex"											//Encodes bytes to hex string
-	"database/sql"											//SQL database
-	"fmt"													//IO stuff
-	"log"													//Logging
-	"net/http"												//HTTP
-	"os"													//Get info from machine
+	  "database/sql"
+    "fmt"
+	  "net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
+    "crypto/sha256"
+    "encoding/hex"
+
+	  "github.com/gin-gonic/gin"
+	  _ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB												//Reference to database, stored globally
-var hasher = sha256.New()									//Hashing function, stored globally
+var db *sql.DB
+var hasher = sha256.New()
 
-type account struct{ 										//Account type, holds a username and password string
-	//ID uint `json:"id"` 									//ig we can use username for id? soemthing about relational algebra?
-	Username string `json:"username"`
-	Password string `json:"password"`
+type account struct{
+    Username string `json:"username"`
+	  Password string `json:"password"`
 }
 
 func main() {
-	//Database stuff
-    cfg := mysql.Config{									//Configure database connection properties
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "softengproj",
-    }
     var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())			//Open Database
-    if err != nil {
-        log.Fatal(err)
-    }
+    db, err = sql.Open("sqlite3", "./database.db")
+    checkErr(err)
 
-    pingErr := db.Ping()									//Try to connect to database
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
+    //insert
+    //inserter, err := db.Prepare("INSERT INTO accounts(username, password) values (?,?)")
+    //checkErr(err)
+
+    //_, err = inserter.Exec("Nido", "1234")
+    //checkErr(err)
+
     fmt.Println("Connected!")
 
-	//HTTP Stuff
-	router := gin.Default() 								//Basic default router
-	
-	router.GET("/", getAccounts) 							//When getting main page, call getAccounts
-	router.POST("/", postAccounts) 							//When posting to main page, call postAccounts
-
-	router.Run("localhost:8080") 							//Run this on port 8080 locally
+    router := gin.Default()
+    router.GET("/", getAccounts)
+    router.Run("localhost:9000")
 }
 
 func getAccounts(con *gin.Context) {
-	var accounts []account
+    var accounts []account
 
-	rows, err := db.Query("SELECT * FROM accounts") 		//Query the accounts table
-	if err != nil{
-		log.Fatal(fmt.Errorf("getAccounts: %v", err))
-		return
-	}
-	
-	defer rows.Close() 										//IDK what this does tbh
-	for rows.Next(){										//Go through the rows, scan them into our account struct, append to list
-		var acc account
-		if err := rows.Scan(&acc.Username, &acc.Password); err != nil{
-			log.Fatal(fmt.Errorf("getAccounts: %v", err))
-			return
-		}
-		accounts = append(accounts, acc)
-	}
+    rows, err := db.Query("SELECT * FROM accounts") 		//Query the accounts table
+    checkErr(err)
 
-	if err := rows.Err(); err != nil{
-		log.Fatal(fmt.Errorf("getAccounts: %v", err))
-		return
-	}
-	con.JSON(http.StatusOK, accounts)						//Send list as json to context
+    for rows.Next(){
+        var acc account
+        err := rows.Scan(&acc.Username, &acc.Password)
+        checkErr(err)
+        accounts = append(accounts, acc)
+    }
+
+    rows.Close()
+
+    con.JSON(http.StatusOK, accounts)
 }
 
-func postAccounts(con *gin.Context){
-	var acc account
-
-	if err := con.BindJSON(&acc); err != nil{				//Bind the curl info to an account
-		log.Fatal(fmt.Errorf("postAccounts: %v", err))
-		return
-	}
-
-	hasher.Reset()											//Clear hashing function
-	hasher.Write([]byte(acc.Username))						//Write info to it
-	acc.Username = hex.EncodeToString(hasher.Sum(nil))		//Get the encrypted value and overwrite (stored as string)
-
-	hasher.Reset()
-	hasher.Write([]byte(acc.Password))
-	acc.Password = hex.EncodeToString(hasher.Sum(nil))
-
-															//Try to insert this into our database
-	result, err := db.Exec("INSERT INTO accounts (username, password) VALUES (?, ?)", acc.Username, acc.Password)
-	if err != nil{
-		log.Fatal(fmt.Errorf("postAccounts: %v", err))
-		return
-	}
-
-	username, err := result.LastInsertId()					//This can probably be removed
-	if err != nil{
-		log.Fatal(fmt.Errorf("postAccounts: %v", err))
-	}
-
-	fmt.Printf("Response: %v\n", username)
-	con.JSON(http.StatusCreated, acc)						//Send out a context message
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
+
+//OLD POST ACCOUNTS, NEED TO WRITE NEW ONE. USE SOMETHING LIKE //insert ABOVE
+
+/*func postAccounts(con *gin.Context){
+    var acc account
+
+    if err := con.BindJSON(&acc); err != nil{				//Bind the curl info to an account
+      log.Fatal(fmt.Errorf("postAccounts: %v", err))
+      return
+    }
+
+    hasher.Reset()											//Clear hashing function
+    hasher.Write([]byte(acc.Username))						//Write info to it
+    acc.Username = hex.EncodeToString(hasher.Sum(nil))		//Get the encrypted value and overwrite (stored as string)
+
+    hasher.Reset()
+    hasher.Write([]byte(acc.Password))
+    acc.Password = hex.EncodeToString(hasher.Sum(nil))
+
+                                //Try to insert this into our database
+    result, err := db.Exec("INSERT INTO accounts (username, password) VALUES (?, ?)", acc.Username, acc.Password)
+    if err != nil{
+      log.Fatal(fmt.Errorf("postAccounts: %v", err))
+      return
+    }
+
+    username, err := result.LastInsertId()					//This can probably be removed
+    if err != nil{
+      log.Fatal(fmt.Errorf("postAccounts: %v", err))
+    }
+
+    fmt.Printf("Response: %v\n", username)
+    con.JSON(http.StatusCreated, acc)						//Send out a context message
+  }*/
