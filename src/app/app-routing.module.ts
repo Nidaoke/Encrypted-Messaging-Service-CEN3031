bc5798ab0a7ccea5299c4fee6c5909f831b332b2
Karen Liu <karen.liu@ufl.edu>
2023-02-26 13:29:27 -0500

    Profile and CORS issue
    Went through some tutorials and tested out the code (they're commented out). Added a profile component because one of them used it and we also need it. There's a bit of a CORS issue when I try to boot it up.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 //https://www.youtube.com/watch?v=f3shwARuhEM&t=424s - guide on creating login
+//https://blog.devgenius.io/angular-login-with-golang-and-mysql-92a59eec1249 - made the first page the profile page (will change later to login)
 const routes: Routes = [
-  {path:'', component:LoginComponent}
+  { path: "", component: LoginComponent },
+  { path: "profile", redirectTo: '/profile', pathMatch: 'full'},
+  {
+    path:'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { JwtInterceptor } from './jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
