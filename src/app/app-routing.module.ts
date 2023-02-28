8eb4c18b94361659d2c8c1d82c8f8046faf2017a
Karen Liu <karen.liu@ufl.edu>
2023-02-28 00:56:11 -0500

    HTTP failed
    Tried connecting to back end but HTTP failure. There's now an errorhandler and authentication thing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './services/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 
@@ -11,7 +11,6 @@ const routes: Routes = [
     path: '',
     component: LoginComponent
   },
-  // { path: "profile", redirectTo: '/profile', pathMatch: 'full'},
   {
     path:'profile',
     component: ProfileComponent,
