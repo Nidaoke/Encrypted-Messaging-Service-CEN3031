f81292c792945fd9f6896d0b2592596a7b9f6733
marcuslugrand <marcuslugrand@ufl.edu>
2023-03-01 15:14:51 -0500

    Test for getAccounts finished

--- web-service-gin/main_test.go
+++ web-service-gin/main_test.go
@@ -6,57 +6,72 @@ import (
 	"testing"
 )
 
-type Tests struct {
-	username string
-	password string
-	email    string
-}
-
 func TestGetAccounts(t *testing.T) {
-	const file string = "database.db"
 
+	const file string = "test.db"
+	const create string = "CREATE TABLE IF NOT EXISTS account (id INT NOT NULL PRIMARY KEY, username VARCHAR(264) NOT NULL,password VARCHAR(264) NOT NULL,email VARCHAR(264) NOT NULL);"
+
+	//Account Sample
 	u := "Marcus"
 	p := "Bloop1"
 	e := "marcus@gmail.com"
 
-	//db, err := sql.Open("sqlite3", file)
-	db, _ := sql.Open("sqlite3", file)
-
-	statement, _ := db.Prepare("CREATE TABLE accounts (username VARCHAR(64) NOT NULL, password VARCHAR(64) NOT NULL, email VARCHAR(64) NOT NULL, PRIMARY KEY (`username`)")
-
-	statement.Exec()
-
-	statement, _ = db.Prepare("INSERT INTO accounts (username, password, email) VALUES (?, ?)")
-
-	statement.Exec(u, p, e)
-
-	rows, _ := db.Query("SELECT username, password, email FROM accounts")
-
-	//var id int
-	var username string
-	var password string
-	var email string
-
-	for rows.Next() {
-		rows.Scan(&username, &password, &email)
-		if u == username && p == password && e == email {
-			fmt.Println("User Found!")
-		} else {
-			t.Fatalf("No User Found")
-		}
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		panic("Failed to connect to database!")
 	}
 
-	/*if _, err := db.Exec(create); err != nil {
-	    return nil, err
-	  }
+	if _, err := db.Exec(create); err != nil {
+		panic(err)
+	}
 
-	  var id int64
-	  if id, err = res.LastInsertId(); err != nil {
-	    fmt.Print("The data base is empty.")
-	  }
+	res, err := db.Exec("INSERT INTO account(id, username, password, email) values(?, ?, ?, ?)", 1, u, p, e)
+	if err != nil {
+		panic(err)
+		//panic("Failed to insert info into database!")
+	}
 
-		fmt.Println(db)
-	*/
+	var ID int64
+	ID, err = res.LastInsertId()
+	if err != nil {
+		panic(err)
+		//panic("Failed to insert info into database!")
+	}
+
+	fmt.Println("Number of accounts: %w", ID)
+
+	rows, err := db.Query("SELECT * FROM account")
+	//rows, err := db.Query("SELECT * username, password, email FROM account")
+	if err != nil {
+		panic("Failed to query into the database!")
+	}
+
+	var scanCheck bool
+
+	for rows.Next() {
+		var id int64
+		var username string
+		var password string
+		var email string
+
+		err = rows.Scan(&id, &username, &password, &email)
+
+		if err != nil {
+			panic(err)
+			//panic("Failed to iterate through the database!")
+		}
+
+		if username == u && p == password && e == email {
+			t.Logf("User %s Found!", username)
+			scanCheck = true
+			break
+		}
+	}
+	rows.Close()
+
+	if scanCheck == false {
+		panic("The user was not found!")
+	}
 }
 
 /*func TestPostAccount(t *testing.T) {
