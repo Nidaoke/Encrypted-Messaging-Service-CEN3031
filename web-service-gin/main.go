4068fb5e551e1e7d5ae2f631ac46a156baa12539
Jonathan Cunningham <jonathan.cunning@ufl.edu>
2023-03-28 14:02:03 -0400

    Added Get and Post methods for global message list

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -18,77 +18,115 @@ var db *sql.DB
 var hasher = sha256.New()
 
 type account struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-  Email string `json:"email"`
+	  Username string `json:"username"`
+	  Password string `json:"password"`
+  	Email string `json:"email"`
+}
+
+type message struct {
+    Id int `json:"id"`
+    From string `json:"from"`
+    To string `json:"to"`
+    Message string `json:"message"`
 }
 
 func main() {
-	var err error
-	db, err = sql.Open("sqlite3", "./database.db")
-	checkErr(err)
+	  var err error
+	  db, err = sql.Open("sqlite3", "./database.db")
+	  checkErr(err)
 
-	//insert
-	//inserter, err := db.Prepare("INSERT INTO accounts(username, password) values (?,?)")
-	//checkErr(err)
+	  //insert
+	  //inserter, err := db.Prepare("INSERT INTO accounts(username, password) values (?,?)")
+	  //checkErr(err)
 
-	//_, err = inserter.Exec("Nido", "1234")
-	//checkErr(err)
+	  //_, err = inserter.Exec("Nido", "1234")
+	  //checkErr(err)
 
-	fmt.Println("Connected!")
+	  fmt.Println("Connected!")
 
-	router := gin.Default()
+	  router := gin.Default()
 
-	//Marcus Added Begin
-	//DefaultConfig as a start point
-	config := cors.DefaultConfig()
-	config.AllowHeaders = []string{"Authorization", "content-type"}
-	config.AllowOrigins = []string{"http://localhost:4200"} //Change to * at some point
-	router.Use(cors.New(config))
-	//End
+	  //Marcus Added Begin
+	  //DefaultConfig as a start point
+	  config := cors.DefaultConfig()
+	  config.AllowHeaders = []string{"Authorization", "content-type"}
+	  config.AllowOrigins = []string{"http://localhost:4200"} //Change to * at some point
+	  router.Use(cors.New(config))
+	  //End
 
-  router.Use(favicon.New("./favicon.ico"));
+  	router.Use(favicon.New("./favicon.ico"));
 
-	router.GET("/", getAccounts)
-	router.POST("/", postAccount)
-	router.Run("localhost:9000")
+	  router.GET("/", getAccounts)
+	  router.POST("/", postAccount)
+    router.GET("/messages", getMessages)
+    router.POST("/messages", postMessage)
+	  router.Run("localhost:9000")
 }
 
 func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+	  if err != nil {
+		    panic(err)
+	  }
 }
 
 func getAccounts(con *gin.Context) {
-	var accounts []account
+	  var accounts []account
 
-	rows, err := db.Query("SELECT * FROM accounts") //Query the accounts table
-	checkErr(err)
+	  rows, err := db.Query("SELECT * FROM accounts") //Query the accounts table
+	  checkErr(err)
 
-	for rows.Next() {
-		var acc account
-		err := rows.Scan(&acc.Username, &acc.Password, &acc.Email)
-		checkErr(err)
-		accounts = append(accounts, acc)
-	}
-	rows.Close()
+	  for rows.Next() {
+		    var acc account
+		    err := rows.Scan(&acc.Username, &acc.Password, &acc.Email)
+		    checkErr(err)
+		    accounts = append(accounts, acc)
+	  }
+	  rows.Close()
 
-	con.JSON(http.StatusOK, accounts)
+	  con.JSON(http.StatusOK, accounts)
 }
 
 func postAccount(con *gin.Context) {
-	var acc account
+	  var acc account
 
-	err := con.BindJSON(&acc)
-	checkErr(err)
+	  err := con.BindJSON(&acc)
+	  checkErr(err)
 
-	hasher.Reset()
-	hasher.Write([]byte(acc.Password))
-	acc.Password = hex.EncodeToString(hasher.Sum(nil))
+	  hasher.Reset()
+	  hasher.Write([]byte(acc.Password))
+	  acc.Password = hex.EncodeToString(hasher.Sum(nil))
 
-	_, err = db.Exec("INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)", acc.Username, acc.Password, acc.Email)
-	checkErr(err)
+	  _, err = db.Exec("INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)", acc.Username, acc.Password, acc.Email)
+	  checkErr(err)
 
-	con.JSON(http.StatusCreated, acc)
+	  con.JSON(http.StatusCreated, acc)
+}
+
+func getMessages(con *gin.Context){
+    var messages []message
+
+    rows, err := db.Query("SELECT * FROM messages") //Query the accounts table
+    checkErr(err)
+
+    for rows.Next() {
+      var mes message
+      err := rows.Scan(&mes.Id, &mes.From, &mes.To, &mes.Message)
+      checkErr(err)
+      messages = append(messages, mes)
+    }
+    rows.Close()
+
+    con.JSON(http.StatusOK, messages)
+}
+
+func postMessage(con *gin.Context) {
+	  var mes message
+
+	  err := con.BindJSON(&mes)
+	  checkErr(err)
+
+	  _, err = db.Exec("INSERT INTO messages VALUES (?, ?, ?, ?)", nil, mes.From, mes.To, mes.Message)
+	  checkErr(err)
+
+	  con.JSON(http.StatusCreated, mes)
 }
