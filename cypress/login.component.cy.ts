654078d347113b98a68413dbce5ba144be3edee4
Karen Liu <karen.liu@ufl.edu>
2023-04-19 21:45:44 -0400

    sign up and login unit test

--- cypress/login.component.cy.ts
+++ cypress/login.component.cy.ts
@@ -8,13 +8,40 @@ it('checks that all input boxes and buttons (signup and login) are visible', () 
     providers: [HttpClient],
   })
 
-    cy.get('input[formcontrolname="username"]').type('UserNameUnitTest')
-    cy.get('input[formcontrolname="username"]').should("be.visible")
-    cy.get('input[formcontrolname="email"]').should("be.visible")
-    cy.get('input[formcontrolname="password"]').should("be.visible")
-    cy.get('button[name="signup"]').should("be.visible")
+  cy.get('input[formcontrolname="username"]').type('UserNameUnitTest')
+  cy.get('input[formcontrolname="username"]').should("be.visible")
+  cy.get('input[formcontrolname="email"]').should("be.visible")
+  cy.get('input[formcontrolname="password"]').should("be.visible")
+  cy.get('button[name="signup"]').should("be.visible")
 
-    cy.get('input[formcontrolname="loginUsername"]').should("be.visible")
-    cy.get('input[formcontrolname="loginPassword"]').should("be.visible")
-    cy.get('button[name="login"]').should("be.visible")
+  cy.get('input[formcontrolname="loginUsername"]').should("be.visible")
+  cy.get('input[formcontrolname="loginPassword"]').should("be.visible")
+  cy.get('button[name="login"]').should("be.visible")
+})
+
+//Unit test to sign up
+it('Sign up', () => {
+    cy.mount(LoginComponent, {
+      imports: [HttpClientModule],
+      declarations: [LoginComponent],
+      providers: [HttpClient],
+    })
+
+    cy.get('input[formcontrolname="username"]').type('UserNameUnitTest')
+    cy.get('input[formcontrolname="email"]').type("usernameunittest@test.com")
+    cy.get('input[formcontrolname="password"]').type("Password123")
+    cy.get('button[name="signup"]').click()
+})
+
+//Continuation of Signup
+it('Login', () => {
+  cy.mount(LoginComponent, {
+    imports: [HttpClientModule],
+    declarations: [LoginComponent],
+    providers: [HttpClient],
   })
+
+  cy.get('input[formcontrolname="loginUsername"]').type('UserNameUnitTest')
+  cy.get('input[formcontrolname="loginPassword"]').type("Password123")
+  cy.get('button[name="login"]').click()
+})
