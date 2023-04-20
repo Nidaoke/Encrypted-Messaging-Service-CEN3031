7999e3cfc2c5fa522fb94c19b90f54a0dc90affe
Karen Liu <73561226+KareO2@users.noreply.github.com>
2023-04-19 22:33:22 -0400

    Update spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,30 @@
 describe('Real e2e testing UwU', () => {
-  it('sign up goes to profile', () => {
+  it('sign up and start talking to Tester2', () => {
     cy.visit('http://localhost:4200/')
     cy.reload()
 
     cy.get('input[formcontrolname="username"]')
-    .type('username2')
+    .type('e2etesting')
 
     cy.get('input[formcontrolname="email"]')
-    .type('nana@emal.com')
+    .type('e2etesting@test.com')
 
     cy.get('input[formcontrolname="password"]')
-    .type('p@55wOrD')
+    .type('p@55wOrD!')
 
     cy.get('button[name="signup"]').click()
 
-    cy.url()
-      .should('be.equal', 'http://localhost:4200/profile')
+    cy.url().should('be.equal', 'http://localhost:4200/profile')
+
+    cy.get('button[name="message"]') // find the button with name "message"
+    .click()
+
+    cy.url().should('be.equal', 'http://localhost:4200/home')
+
+    cy.get('input[formcontrolname="messageControll"]')
+    .type("Cypress E2E says Hello")
+    .trigger('keydown', {
+      key: 'Enter',
+    });
   })
 })
